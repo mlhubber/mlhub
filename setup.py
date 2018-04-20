6c8c885b86a057c52c13fd376deb747fecbf1bf5
Graham Williams <Graham.Williams@togaware.com>
2018-04-21 05:31:00 +0800

    Prepare for pypi upload.

--- setup.py
+++ setup.py
@@ -22,166 +22,23 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
-
 setup(
-    # This is the name of my project. The first time I publish this
-    # package, this name will be registered for me. It will determine 
-    # how users can install this project, e.g.:
-    #
-    # $ pip install mlhub
-    #
-    # Also where it will live on PyPI:
-    # https://pypi.org/project/mlhub/
-    #
-    # There are some restrictions on what makes a valid project name
-    # specification here:
-    #    https://packaging.python.org/specifications/core-metadata/#name
-    
-    name='mlhub',  # Required
-    
-    # Versions should comply with PEP 440:
-    # https://www.python.org/dev/peps/pep-0440/
-    #
-    # For a discussion on single-sourcing the version across setup.py and the
-    # project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    
-    version='1.0.4',  # Required. DO NOT MODIFY. Managed from Makefile.
-    
-    # This is a one-line description or tagline of what the project does. This
-    # corresponds to the "Summary" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#summary
-    
-    description='Machine learning model repository manager',  # Required
-    
-    # This is an optional longer description of the project that
-    # represents the body of text which users will see when they visit
-    # PyPI.
-    #
-    # Often, this is the same as the README, so we just read it in
-    # from that file directly (as we have already done above)
-    #
-    # This field corresponds to the "Description" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#description-optional
-    
-    long_description=long_description,  # Optional
-    
-    # This should be a valid link to the project's main homepage.
-    #
-    # This field corresponds to the "Home-Page" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    
-    url='https://mlhub.ai',  # Optional
-    
-    # This should be the name of the organization (or person) which
-    # owns the project.
-    
-    author='Open Source',  # Optional
-    
-    # This should be a valid email address corresponding to the author listed
-    # above.
-    
-    author_email='mlhub@togaware.com',  # Optional
-    
-    # Classifiers help users find the project by categorizing it.
-    #
-    # For a list of valid classifiers, see
-    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-        
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Machine Learning Pre-Built Models :: ML Models',
-        
-        # Pick your license as you wish (should match "license" above)
-
-        'License :: OSI Approved :: GPL License',
-        
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-
-        #'Programming Language :: Python :: 2',
-        #'Programming Language :: Python :: 2.7',
+    name='mlhub',
+    version='1.0.11',  # DO NOT MODIFY. Managed from Makefile.
+    description='Machine learning model repository manager',
+    long_description=long_description,
+    author='Graham Williams',
+    author_email='mlhub@togaware.com',
+    url='https://mlhub.ai',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
     ],
-    
-    # This field adds keywords for your project which will appear on the
-    # project page. What does your project relate to?
-    #
-    # Note that this is a string of words separated by whitespace, not a list.
-    keywords='sample setuptools development',  # Optional
-    
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
-    
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    #install_requires=['peppercorn'],  # Optional
-    
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). Users will be able to install these using the "extras"
-    # syntax, for example:
-    #
-    #   $ pip install sampleproject[dev]
-    #
-    # Similar to `install_requires` above, these must be valid existing
-    # projects.
-    extras_require={  # Optional
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
-    
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.
-    #
-    # If using Python 2.6 or earlier, then these have to be included in
-    # MANIFEST.in as well.
-    
-    package_data={  # Optional
-        'mlhub': ['package_data.dat'],
-    },
-    
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    #
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],  # Optional
-    
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `ml`
-    # which executes the function `main` from this package when
-    # invoked:
-    
-    entry_points={  # Optional
-        'console_scripts': [
-            'ml=mlhub:main',
-        ],
-    },
+    keywords='machine learning models repository',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    package_data={'.': ['LICENSE']},
+    entry_points={'console_scripts': ['ml=mlhub:main']},
 )
