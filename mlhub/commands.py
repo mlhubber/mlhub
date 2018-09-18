b3d069d6a22c6408a677ff19b9598680ddc4ca7f
Graham Williams <graham.williams@togaware.com>
2018-09-18 13:43:47 +0800

    Update message formatting to improve end user readability.

--- mlhub/commands.py
+++ mlhub/commands.py
@@ -518,8 +518,10 @@ def dispatch(args):
         sys.stdout.write(msg)
         choice = input().lower()
         if choice != 'y':
-            msg = """To enable DISPLAY be sure to connect to the server using 'ssh -X'
+            msg = """
+To enable DISPLAY be sure to connect to the server using 'ssh -X'
 or else connect to the server's desktop using a local X server like X2Go.
+
 """
             sys.stdout.write(msg)
             sys.exit(1)
