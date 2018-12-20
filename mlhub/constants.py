f045329c88489af0310a78fc230af29bb0cf7de8
Graham Williams <graham.williams@togaware.com>
2018-12-20 15:35:03 +0800

    Bump version 2.9.10 - updated messaging

--- mlhub/constants.py
+++ mlhub/constants.py
@@ -84,7 +84,7 @@ CMD = "ml"                 # The command line tool.
 EXT_MLM = ".mlm"    # Archive filename extension
 EXT_AIPK = ".aipk"  # Backward compatibility
 
-VERSION = "2.9.8"  # DO NOT MODIFY. Managed from ../Makefile.
+VERSION = "2.9.10"  # DO NOT MODIFY. Managed from ../Makefile.
 
 # Global command line options
 
