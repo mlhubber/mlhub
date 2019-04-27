5410729f8f684f9b292ce8162e480a6eb499853d
Graham <Graham.Williams@togaware.com>
2019-04-27 13:19:25 +0800

    Re

---
+++ R/mlpreview.R
@@ -0,0 +1,18 @@
+#' Preview File
+#'
+#' Display PDF file.
+#' @param fname File name to display
+#' @param msg Message to display after displaying the file.
+#' @param previewer The command to run to display the file.
+#' @keywords display
+#' @export
+#' @examples
+#' mlpreview()
+ 
+mlpreview <- function(fname,
+                      msg="Close the graphic window using Ctrl-w.\n",
+                      previewer="atril --preview")
+{
+  system(paste(previewer, fname), ignore.stderr=TRUE, wait=FALSE)
+  cat(msg)
+}
