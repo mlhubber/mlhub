147f022d5ba1878c4b234b1588536ac871de6470
Graham Williams <Graham.Williams@togaware.com>
2020-06-27 14:21:56 +1000

    Check if errors is non empty when return code is non-zero.

--- mlhub/commands.py
+++ mlhub/commands.py
@@ -1057,7 +1057,7 @@ or else connect to the server's desktop using a local X server like X2Go.
     proc = subprocess.Popen(command, shell=True, cwd=path)
     output, errors = proc.communicate()
     missing_r_dep = False
-    if proc.returncode != 0:
+    if proc.returncode != 0 and errors:
         errors = errors.decode("utf-8")
 
         # Check if it is Python dependency unsatisfied
