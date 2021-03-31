1fee5e37d23ff719d62cf0aa7719f373162b7cc8
Graham Williams <Graham.Williams@togaware.com>
2021-03-31 12:19:45 +1100

    Resync. Remove app and survivor into own packages, update R mlcat() to import glue.

--- R/mlcat.R
+++ R/mlcat.R
@@ -1,20 +1,21 @@
 #' Inform User
 #'
 #' Display an informative message.
+#' 
 #' @param title The section title after processing with glue.
 #' @param text The text to display after processing with glue.
 #' @param delim Delimiter to highlight the title.
 #' @param begin Text at the beginning.
 #' @param end Text at the end.
+#'
+#' @importFrom glue glue
 #' @keywords informative
 #' @export
-#' @examples
-#' mlcat()
  
 mlcat <- function(title="", text="", delim="=", begin="", end="\n")
 {
-  title <- glue::glue(title)
-  text <- paste(strwrap(glue::glue(text, .trim=FALSE)), collapse="\n")
+  title <- glue(title)
+  text <- paste(strwrap(glue(text, .trim=FALSE)), collapse="\n")
   sep <- paste0(paste(rep(delim, nchar(title)), collapse=""),
                 ifelse(nchar(title) > 0, "\n", ""))
   ttl_sep <- ifelse(nchar(title) > 0, "\n", "")
