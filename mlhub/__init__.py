176f632c1566f3c8aac27bbb8e25c38e2db39db9
Graham Williams <Graham.Williams@togaware.com>
2021-04-29 13:31:23 +1000

    Remove a couple of long lines.

--- mlhub/__init__.py
+++ mlhub/__init__.py
@@ -10,12 +10,13 @@
 #
 # MIT License
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the ""Software""), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the ""Software""), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
@@ -55,7 +56,8 @@ utils.add_log_handler(
     constants.LOG_FILE_LEVEL,
     constants.LOG_FILE_FORMAT)
 
-logger.info('---------- {} {} ----------'.format(os.path.basename(sys.argv[0]), ' '.join(sys.argv[1:])))
+logger.info('---------- {} {} ----------'.format(os.path.basename(sys.argv[0]),
+                                                 ' '.join(sys.argv[1:])))
 
 # ----------------------------------------------------------------------
 # Set up command line parser and dispatch commands.
